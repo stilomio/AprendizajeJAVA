4455bb7ae62aec9add5aef97d34ec24eb4c7c753
Jose Velasquez <shop.velasquez@gmail.com>
2015-01-01 17:58:46 -0430

    Videotutorial 2

---
+++ VideotutorialJava/src/anatomia/Anatomia.java
@@ -0,0 +1,9 @@
+
+class Anatomia {
+
+    public static void main(String[] args) {
+   //Mi primer Programa Java va aqui
+        String saludo  = "Bienvenidos al Curso de Videotutoriales de Java!";
+    }
+
+}
